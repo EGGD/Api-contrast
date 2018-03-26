5f9d7bb4182b0a51e0f4d3cd293eeb4355df8bfb
DESKTOP-NEDMMDF\CL <772319837@qq.com>
2018-03-26 17:13:30 +0800

    一部分的bug修复

--- src/conpment/TreeItem.js
+++ src/conpment/TreeItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Tree } from 'antd';
+import { Object } from 'core-js';
 const TreeNode = Tree.TreeNode;
 
 class TreeItem extends Component {
@@ -12,10 +13,8 @@ class TreeItem extends Component {
     //当字段为字符串类型就调用下面的字符串方法
     if (typeof (data) === 'object') {
       list.push(
-        // <TreeNode title="" key={`${Math.random(100)}`} >
         this.onShowObj(data, flgdata)
-        // </TreeNode>
-      )
+      );
     } else {
       for (let i = 0; i < data.length; i++) {
         let temp = data[i];
@@ -26,35 +25,37 @@ class TreeItem extends Component {
             const flgelement = flgtemp.hasOwnProperty(key) ? flgtemp[key] : null;
             if (typeof (element) === 'object') {
               list.push(
-                // <TreeNode title={`${key}`} key={`${i}${Math.random(100)}`} >
                 this.onShowTreeNodeChild(element, flgelement)
-                // </TreeNode>
-              )
+              );
             } else {
               if (element === flgelement) {
-                list.push(<TreeNode title={`${key}:${element}`} key={`${i}${Math.random(100)}`} />)
+                list.push(<TreeNode title={`${key}:${element}`} key={`${i}${Math.random(100)}`} />);
               } else {
                 list.push(<TreeNode title={
                   <span style={{ color: '#1890ff' }}>{`${key}:${element}`}</span>
-                } key={`${i}${Math.random(100)}`} />)
-              }
-            }
-          }
-        }
-      }
-    }
+                } key={`${i}${Math.random(100)}`} />);
+              };
+            };
+          };
+        };
+      };
+    };
     return list;
-  }
+  };
   onShowObj(data, flgdata) {
     let list = [];
+    let flg;
     for (const key in data) {
       if (data.hasOwnProperty(key)) {
         const element = data[key];
         //判断新加语句的时候直接就返回当前语句
         if (flgdata === null && data !== '') {
-          list.push(<TreeNode title={
-            <span style={{ color: '#1890ff' }}>{`${key}:${element}`}</span>
-          } key={`${key}${Math.random(100)}`} />)
+          if (flg !== data) {
+            list.push(
+                this.allNewObj(data)
+            );
+          };
+          flg = data;
         } else {
           const flgelement = flgdata.hasOwnProperty(key) ? flgdata[key] : null;
           if (typeof (element) === 'object' && element !== null) {
@@ -62,20 +63,40 @@ class TreeItem extends Component {
               <TreeNode title={`${key}`} key={`${key}${Math.random(100)}`} >
                 {this.onShowTreeNodeChild(element, flgelement)}
               </TreeNode>
-            )
+            );
           } else {
             if (element === flgelement) {
-              list.push(<TreeNode title={`${key}:${element}`} key={`${key}${Math.random(100)}`} />)
+              list.push(<TreeNode title={`${key}:${element}`} key={`${key}${Math.random(100)}`} />);
             } else {
               list.push(<TreeNode title={
                 <span style={{ color: '#1890ff' }}>{`${key}:${element}`}</span>
-              } key={`${key}${Math.random(100)}`} />)
-            }
-          }
-        }
-
-      }
-    }
+              } key={`${key}${Math.random(100)}`} />);
+            };
+          };
+        };
+      };
+    };
+    return list;
+  }
+  allNewObj(data) {
+    let list = [];
+    if (data instanceof Array) {
+      for (let i = 0; i < data.length; i++) {
+        let temp = data[i];
+        list.push(
+          <TreeNode title={`${i}`} key={`${i}${Math.random(100)}`} >
+            {this.allNewObj(temp)}
+          </TreeNode>
+        );
+      };
+    } else {
+      for (const key in data) {
+        const element = data[key];
+        list.push(<TreeNode title={
+          <span style={{ color: '#1890ff' }}>{`${key}:${element}`}</span>
+        } key={`${key}${Math.random(100)}`} />);
+      };
+    };
     return list;
   }
   onSelect(selectedKeys, info) {
